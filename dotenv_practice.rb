71042d006faf59fbde126baf1c8dca839f79cad4
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-09 14:01:14 -0700

    requires dotenv and httparty gems, sets up authentication, searches for Parasite from TMDB

---
+++ dotenv_practice.rb
@@ -0,0 +1,19 @@
+# dotenv_practice.rb
+require 'dotenv'
+require 'httparty'
+
+# Tell dotenv to look for the .env file
+Dotenv.load
+
+url = "https://api.themoviedb.org/3/search/movie"
+
+params = {
+	query: "기생충",
+	api_key: ENV['API_KEY']
+}
+
+response = HTTParty.get(url, query: params)
+
+p response 
+# puts ENV['LUNCH']
+# # => from .env
